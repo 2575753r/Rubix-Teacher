ab59180ecd803a7a5f28a246d14ddba83850fa83
Nathan Roulson <n_roulson@outlook.com>
2024-12-28 09:47:46 +0000

    tests involving rendering removed

--- __tests__/Cube.test.js
+++
@@ -1,6 +0,0 @@
-import { render, fireEvent } from '@testing-library/react';
-import Cube from './Cube';
-
-test('renders the cube', () => {
-    const { container } = render(<Cube />);
-});
--- __tests__/HomePage.test.js
+++
@@ -1,6 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import HomePage from './HomePage';
-
-test('renders the home page with a cube', () => {
-    render(<HomePage />);
-});
