a951fae1aa62718c3b078fb5d30ece49ab56f3a1
Nathan Roulson <n_roulson@outlook.com>
2025-01-03 19:54:18 +0000

    Backend recieves dictionary of matrices and processes them to be used in the algorithm closes #18

--- flaskBackend/matrixProcesser.py
+++ flaskBackend/matrixProcesser.py
@@ -1,6 +1,27 @@
-def processMatrix(matrix):
-    
+def processMatrices(matrices):
+    print('processing matrices')
+    face_order = ['up', 'left', 'front', 'right', 'back', 'down']
+    cubeLayout = ''.join(extractColours(matrices[face]) for face in face_order)
 
 
+    return cubeLayout
 
-    return
+def extractColours(matrix):
+
+    faceLayout = ''
+    for row in matrix:
+        for colour in row:
+            if colour == 'yellow':
+                faceLayout = faceLayout + 'y'
+            if colour == 'blue':
+                faceLayout = faceLayout + 'b'
+            if colour == 'red':
+                faceLayout = faceLayout + 'r'
+            if colour == 'green':
+                faceLayout = faceLayout + 'g'
+            if colour == 'orange':
+                faceLayout = faceLayout + 'o'
+            if colour == 'white':
+                faceLayout = faceLayout + 'w'
+
+    return faceLayout
